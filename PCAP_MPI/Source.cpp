eb2162d104ffd6ab9a9673b2c70f59588303bcab
zeokav <sakettsingh@outlook.com>
2017-02-26 17:48:31 +0530

    Added opencv

--- PCAP_MPI/Source.cpp
+++ PCAP_MPI/Source.cpp
@@ -1,12 +1,26 @@
 #include "mpi.h"
-#include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
 
 int main(int argc, char *argv[]) {
 	int rank, size;
+	
+	// Read file specified in argument
+	Mat img;
+	cout << argv[1] << endl;
+	img = imread(argv[1], 1);
+	namedWindow("Rachit", WINDOW_AUTOSIZE);
+	imshow("Rachit", img);
+	waitKey(0);
+
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	printf("Rank: %d\n", rank);
+	printf("Rank: %s\n", argv[1]);
 	MPI_Finalize();
 }
