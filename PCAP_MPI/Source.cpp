270c09c0cec4a6faa88c5948a68904cfb7d0b36a
zeokav <sakettsingh@outlook.com>
2017-03-05 02:25:33 +0530

    Some weird shit happening

--- PCAP_MPI/Source.cpp
+++ PCAP_MPI/Source.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	int *final_blue;
 	int cols, rows, sstr_len;
 	char str[1000], recv_str[1000];
-	int msg_len;
+	int msg_len, store;
 	Mat img;
 
 	// Init MPI
@@ -35,18 +35,17 @@ int main(int argc, char *argv[])
 	MPI_Bcast(&rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 	img_blue = new int[rows * cols];
-	recv_img_blue = new int[cols];
-	final_blue = new int[size * cols];
+	recv_img_blue = new int[cols * rows];
+	final_blue = new int[rows * cols];
 
 	if(rank == 0) 
 	{
-
 		cout << "Enter the message to be hidden: ";
 		cin >> str;
 		msg_len = strlen(str);
 		sstr_len = msg_len / size;
 
-		// Storing in seperate matrices for each color value
+		// Storing blue values in a single dimension array.
 		for (i = 0; i < rows; i++)
 		{
 			for (j = 0; j < cols; j++)
@@ -59,11 +58,14 @@ int main(int argc, char *argv[])
 
 		k = 0;
 		flag = 0;
-
+		store = msg_len;
+		// Burn the size of the message to be written at the end of the image. 
+		// This is used to extract how many bytes were written (say n) - from the encoded image.
+		// Using this, we can decode the first n/size values from each row
 		for (i = rows - 1; i >= 0 && !flag; i--) {
 			for (j = cols - 1; j >= 0 && !flag; j--) {
-				img.at<Vec3b>(i, j).val[0] = ((img.at<Vec3b>(i, j).val[0] >> 2) << 2) | (msg_len & 0x3);
-				msg_len = msg_len >> 2;
+				img.at<Vec3b>(i, j).val[0] = ((img.at<Vec3b>(i, j).val[0] >> 2) << 2) | (store & 0x3);
+				store = store >> 2;
 				k += 2;
 				if (k == 16) {
 					flag = 1;
@@ -71,30 +73,43 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
-
 	MPI_Bcast(&sstr_len, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	// Scatter arrays
-	MPI_Scatter(img_blue, cols, MPI_INT, recv_img_blue, cols, MPI_INT, 0, MPI_COMM_WORLD);
+	// Scatter blue value array - 4 * number of bytes to be written by each process
+	MPI_Scatter(img_blue, sstr_len * 4, MPI_INT, recv_img_blue, sstr_len * 4, MPI_INT, 0, MPI_COMM_WORLD);
+
+	// Scatter string - number of bytes written by each process
 	MPI_Scatter(str, sstr_len, MPI_CHAR, recv_str, sstr_len, MPI_CHAR, 0, MPI_COMM_WORLD);
 
-	int row_base = cols * rank;
+	int _base = sstr_len * rank * 4;
 	for (i = 0; i < sstr_len && recv_str[i] != '\0'; i++) {
 		char curr = recv_str[i];
 		for (j = 0; j < sizeof(char) * 4; j++) {
-			int pos = row_base + (4 * i) + j;
+			int pos = _base + (4 * i) + j;
+			cout << "In rank " << rank << ", old: " << recv_img_blue[pos] << endl;
 			recv_img_blue[pos] = ((recv_img_blue[pos] >> 2) << 2) | (curr & 0x3);
+			cout << "In rank " << rank << ", new: " << recv_img_blue[pos] << endl;
 			curr = curr >> 2;
 		}
 	}
 
-	MPI_Gather(recv_img_blue, cols, MPI_INT, final_blue, cols, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Gather(recv_img_blue, sstr_len * 4, MPI_INT, final_blue, sstr_len * 4, MPI_INT, 0, MPI_COMM_WORLD);
 
 	if (rank == 0) {
-		for (i = 0; i < size; i++) {
-			for (j = 0; j < cols; j++) {
-				img.at<Vec3b>(i, j).val[0] = final_blue[(i * cols) + j];
+
+		for (i = 0; i < 30; i++) {
+			cout << i << ", old: " << img_blue[i] << ", new: " << final_blue[i] << endl;
+		}
+
+		int off = 0;
+		k = 0;
+		cout << msg_len;
+		for (i = 0; i < msg_len * 4; i++, off++) {
+			if (off >= cols) {
+				off = 0;
+				k++;
 			}
+			img.at<Vec3b>(k, off) = final_blue[i];
 		}
 		namedWindow("Saved File", WINDOW_AUTOSIZE);
 		imshow("Saved File", img);
